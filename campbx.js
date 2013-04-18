78a7513ec595f44bfdca76adf38cd78a4a53dc79
Preston Skupinski <preston.skupinski@gmail.com>
2013-04-18 00:59:37 -0400

    add support for trade and account methods

--- campbx.js
+++ campbx.js
@@ -1,8 +1,11 @@
 var request = require("request");
 
-var CampBX = function() {
+var CampBX = function(username, password) {
   var self = this;
   self.publicUrl = "http://campbx.com/api/";
+  self.url = "https://campbx.com/api/";
+  self.username = username;
+  self.password = password;
 
   self.makePublicRequest = function(method, callback) {
     request(self.publicUrl + method, function(err, response, body) {
@@ -15,12 +18,74 @@ var CampBX = function() {
     });
   };
 
+  self.makeRequest = function(method, params, callback) {
+    params.user = self.username;
+    params.pass = self.password;
+    request({ url: self.url + method, method: "POST", form: params }, function(err, response, body) {
+      if(err || response.statusCode !== 200) {
+        callback(err ? err : response.statusCode);
+        return;
+      }
+
+      var result = JSON.parse(body);
+
+      if(result.error) {
+        callback(result.error);
+        return;
+      }
+
+      callback(false, result);
+    });
+  };
+
   self.depth = function(callback) {
     self.makePublicRequest("xdepth.php", callback);
   };
 
   self.ticker = function(callback) {
     self.makePublicRequest("xticker.php", callback);
+  };
+
+  self.myFunds = function(callback) {
+    self.makeRequest("myfunds.php", {}, callback);
+  };
+
+  self.myOrders = function(callback) {
+    self.makeRequest("myorders.php", {}, callback);
+  };
+
+  self.myMargins = function(callback) {
+    self.makeRequest("mymargins.php", {}, callback);
+  };
+
+  self.getBtcAddr = function(callback) {
+    self.makeRequest("getbtcaddr.php", {}, callback);
+  };
+
+  self.sendInstant = function(CBXCode, btcAmount, callback) {
+    self.makeRequest("sendinstant.php", { "CBXCode":CBXCode, "BTCAmt": btcAmount }, callback);
+  };
+
+  self.sendBtc = function(btcTo, btcAmount, callback) {
+    self.makeRequest("sendbtc.php", { "BTCTo": btcTo, "BTCAmt": btcAmount }, callback);
+  };
+
+  self.tradeCancel = function(type, orderId, callback) {
+    self.makeRequest("tradecancel.php", { "Type": type, "OrderId": orderId }, callback);
+  };
+
+  self.tradeEnter = function(tradeMode, quantity, price, callback) {
+    self.makeRequest("tradeenter.php", { "TradeMode": tradeMode, "Quantity": quantity, "Price": price }, callback);
+  };
+
+  self.tradeAdv = function(tradeMode, price, quantity, optionalFields, callback) {
+    var params = { "TradeMode": tradeMode, "Price": price, "Quantity": quantity };
+
+    for(var k in optionalFields) {
+      params[k] = optionalFields[k];
+    }
+
+    self.makeRequest("tradeadv.php", params, callback);
   };
 };
 
